9be37e0849eb52141becfd76fafb2388ccb60eac
George Velentzas <geovelentzas@gmail.com>
2017-12-02 12:15:50 +0200

    Add means of gaussians for exploration and q-values

--- RunSession.m
+++ RunSession.m
@@ -18,11 +18,17 @@ Sigmas = nan*zeros(BBT.nA, episodeLength+1, BBT.nS);            %tracking gaussi
 Betas = zeros(BBT.nS, episodeLength+1);                         %tracking inverse temperature (nSxT)
 Engagement = zeros(1,episodeLength);                            %tracking engagement (1xT)
 Metaparams = zeros(BBT.nA, episodeLength+1,BBT.nS);             %tracking metaparams (nAxTxnS)
+ExpectedActionParams = zeros(BBT.nA, episodeLength+1, BBT.nS);  %tracking means of gaussians (nAxTxnS)
+Qvalues = zeros(BBT.nA, episodeLength+1, BBT.nS);               %tracking Qvalues (nAxTxnS)
+
+
 Sigmas(:, 1, :) = BBR.SIGMAS2';                                 %gaussian exploration stds at first timestep
 Betas(:, 1) = BBR.BETAS;                                        %betas at first timestep
 Engagement(1, 1) = BBT.cENG;                                    %initial engagement
 Metaparams(:, 1, :) = BBR.METAPARAMS2';                         %initial metaparams values
 StatesVisited = s;                                              %track States visited in a list (array)
+ExpectedActionParams(:,1,:) = BBR.wA';                          %track means of gaussians for exploration
+Qvalues(:,1,:) = BBR.Q';                                        %track Qvalues
 
 %% Run Task ... Decide-Transition-Observe-Learn and Track History
 for iii=1:episodeLength
@@ -34,6 +40,8 @@ for iii=1:episodeLength
     Metaparams(:,iii+1, :) = BBR.METAPARAMS2';                  %track values of metaparameters
     OptimalActions(BBT.optimal(s), iii+1, s) = BBT.engMu(s);    %track Optimal Actions
     StatesVisited(iii+1) = s;                                   %track visited States
+    ExpectedActionParams(:,iii+1,:) = BBR.wA';                  %track estimated parameter means
+    Qvalues(:, iii+1, :) = BBR.Q';                              %track Qvalues
 end
 
 %% Save Session variables
@@ -46,5 +54,7 @@ Sess.StatesVisited = StatesVisited;                             %StatesVisited: 
 Sess.Metaparams = Metaparams;                                   %Metaparams: (nAxTxnS)
 Sess.Hits = BBR.Hits;                                           %Hits: (1xT)
 Sess.DHits = BBR.DHits;                                         %DHits: (1xT)
+Sess.ExpectedActionParams = ExpectedActionParams;               %ExpParamVal: (nAxTxnS);
+Sess.Qvalues = Qvalues;                                         %Qvalues: (nAxTxnS);
 end
 
