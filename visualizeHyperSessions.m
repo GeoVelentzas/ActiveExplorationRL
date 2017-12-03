09b7fab4d3ba7bf7adc2b2c29618478008b75c3f
George Velentzas <geovelentzas@gmail.com>
2017-12-03 12:17:54 +0200

    Add visualizations for beta-sigma-metaparams-qvalues

--- visualizeHyperSessions.m
+++ visualizeHyperSessions.m
@@ -17,6 +17,7 @@ for action = 1:6
     Actions{action} = Hsess.ActionsTaken(n{action}, :, :);                      %Actions{a}: (nAxnexp)xTxnS 
     Sigmas{action} = Hsess.Sigmas(n{action}, :,:);                              %Sigmas{a}:  (nAxnexp)xTxnS
     Metaparams{action} = Hsess.Metaparams(n{action}, :, :);                     %Metaparams{a}: (nAxnexp)xTxnS
+    Qvalues{action} = Hsess.Qvalues(n{action}, :, :);                     %Qvalues{a}: (nAxnexp)xTxnS
     ExpectedActionParams{action} = Hsess.ExpectedActionParams(n{action},:,:);   %ExpActPar{a}: (nAxnexp)xTxnS
     OptimalActions{action} = Hsess.OptimalActions(n{action}, :, :);             %OptAct{a}: (nAxnexp)xTxnS
 end
@@ -24,7 +25,7 @@ for state = 1:5
     m{state} = state:5:nexp*5;                                                  %indexes for each state
     Betas{state} = Hsess.Betas(m{state}, :);                                    %Betas{s}: nexpxT
 end
-
+drawnow;
 %% actions taken at each state and optimal actions with tolerance interval
 figure(1); suptitle('actions taken and optimal actions')
 for state = 1:5
@@ -49,7 +50,7 @@ for state = 1:5
     end
     xlim([0 T]); ylim([-100 100]);
 end
-
+drawnow;
 %% estimated means of gaussian exploration for optimal actions + 3*sigmas
 figure(1);
 for state=1:5
@@ -65,33 +66,99 @@ for state=1:5
     end
     xlim([0 T]); ylim([-100 100]);
 end
-
-%% engagement
+drawnow;
+%% engagement - probabilities
 figure(2);
 q1 = quantile(Hsess.Engagement, 0.25);
 q2 = quantile(Hsess.Engagement, 0.5);
 q3 = quantile(Hsess.Engagement, 0.75);
-subplot(3,1,1);
+
+% engagement
+subplot(1,2,1);
 plot(q2, 'k'); hold on; box on;
 t = 1:T;
 x = [t fliplr(t)];
 y = [q1 fliplr(q3)];
 fill(x, y, 'k', 'EdgeColor', 'None'); alpha(0.5);
-xlim([0 T]);
+xlim([0 T]); ylim([0 10]);
 
-
-
-
-
-
-
-
-
-
-
-
-
+%probabilities
+subplot(1,2,2);
+phit = mean(Hsess.Hits, 1);
+plot(smooth(phit), 'k'); hold on; box on;
+xlim([0 T]); ylim([0 1]);
+pdhit = mean(Hsess.DHits, 1);
+plot(smooth(pdhit), 'r')
+drawnow;
+%% sigmas - betas
+figure(3);
+for state=1:5
+    subplot(5,2,state*2-1);
+    for action=1:6
+        q1 = quantile(Sigmas{action}(:,:,state), 0.25);
+        q2 = quantile(Sigmas{action}(:,:,state), 0.5);
+        q3 = quantile(Sigmas{action}(:,:,state), 0.75);
+        plot(q2, '-', 'Color', b(action,:), 'LineWidth', 2); hold on; box on;
+        t = 1:T; x = [t fliplr(t)]; y = [q1 fliplr(q3)];
+        fill(x,y,b(action,:), 'EdgeColor', 'None'); alpha(0.4);
+        xlim([0 T]);
+    end
+    subplot(5,2,state*2);
+    q1 = quantile(Betas{state}, 0.25);
+    q2 = quantile(Betas{state}, 0.5);
+    q3 = quantile(Betas{state}, 0.75);
+    plot(q2, 'k-', 'LineWidth', 2); hold on; box on;
+    t = 1:T; x = [t fliplr(t)]; y = [q1 fliplr(q3)];
+    fill(x,y, 'k', 'EdgeColor', 'None'); alpha(0.4); 
+    xlim([0 T]);
+end
+drawnow;
+%% metaparams - q-values
+figure(4);
+for state=1:5
+    subplot(5,2,state*2-1);
+    for action=1:6
+        q1 = quantile(Metaparams{action}(:,:,state), 0.25);
+        q2 = quantile(Metaparams{action}(:,:,state), 0.5);
+        q3 = quantile(Metaparams{action}(:,:,state), 0.75);
+        plot(q2, '-', 'Color', b(action,:), 'LineWidth', 2); hold on; box on;
+        t = 1:T; x = [t fliplr(t)]; y = [q1 fliplr(q3)];
+        fill(x,y,b(action,:), 'EdgeColor', 'None'); alpha(0.4);
+        xlim([0 T]);
+    end
+    subplot(5,2,state*2);
+    for action=1:6
+        q1 = quantile(Qvalues{action}(:,:,state), 0.25);
+        q2 = quantile(Qvalues{action}(:,:,state), 0.5);
+        q3 = quantile(Qvalues{action}(:,:,state), 0.75);
+        plot(q2, '-', 'Color', b(action,:), 'LineWidth', 2); hold on; box on;
+        t = 1:T; x = [t fliplr(t)]; y = [q1 fliplr(q3)];
+        fill(x,y,b(action,:), 'EdgeColor', 'None'); alpha(0.4);
+        xlim([0 T]);
+    end
+    
 
 
 end
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
