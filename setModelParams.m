80fb5fcf0782b9ecd24a4e9d6a4a7496be7278f1
George Velentzas <geovelentzas@gmail.com>
2017-12-06 15:26:58 +0200

    Add function to set model parameters

---
+++ setModelParams.m
@@ -0,0 +1,12 @@
+function BBR = setModelParams( BBR, aC, aA, gamma, mu, tau1, tau2, gainSigma )
+
+BBR.alphaC = aC;
+BBR.alphaA = aA;
+BBR.gamma = gamma;
+BBR.mu = mu;
+BBR.tau1 = tau1;
+BBR.tau2 = tau2;
+BBR.gainSigma = gainSigma;
+
+end
+
