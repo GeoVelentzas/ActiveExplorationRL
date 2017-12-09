26c393c1afede904dcd86563fddca5622f81af25
George Velentzas <geovelentzas@gmail.com>
2017-12-09 18:17:08 +0200

    Add stochastic cases and not known task case

--- BBsetParams.m
+++ BBsetParams.m
@@ -1,29 +1,23 @@
-function BBR = BBsetParams( task )
+function BBR = BBsetParams( BBR, task )
 
 switch task
-    case 'non-stationary2'
-        BBR.aC=0.1; 
-        BBR.aA=0.5; 
+    case {'non-stationary2', 'stochastic-non-stationary2', 'non-stationary1', 'stochastic-non-stationary1'}
+        BBR.alphaC=0.1; 
+        BBR.alphaA=0.5; 
         BBR.gamma=0.7; 
         BBR.mu=0.1; 
         BBR.tau1=10; 
         BBR.tau2=5; 
         BBR.gainSigma=20;
-    case 'non-stationary1'
-        BBR.aC=0.1; 
-        BBR.aA=0.5; 
-        BBR.gamma=0.7; 
-        BBR.mu=0.1; 
-        BBR.tau1=10; 
-        BBR.tau2=5; 
-        BBR.gainSigma=20;
-    case 'non-stationary0'
-        BBR.aC=0.5; 
-        BBR.aA=0.2; 
+    case {'non-stationary0', 'stationary', 'stochastic-non-stationary0'}
+        BBR.alphaC=0.5; 
+        BBR.alphaA=0.2; 
         BBR.gamma=0.95; 
         BBR.mu=0.2;                             
         BBR.tau1=10; 
         BBR.tau2=5; 
         BBR.gainSigma=20;
+    otherwise
+        warning('not a known task type')
 end
 
