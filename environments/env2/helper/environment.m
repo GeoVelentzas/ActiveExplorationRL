ad08d4d13175d656d679641d774d2bbfb99b2885
George Velentzas <geovelentzas@gmail.com>
2018-01-22 22:45:38 +0200

    Add a random choice example of transitions

--- environments/env2/helper/environment.m
+++ environments/env2/helper/environment.m
@@ -1,17 +1,22 @@
 classdef environment
     properties
-        T; s; sp;
+        %T: Transition Matrix
+        %O: Optimal Actions-Params
+        T; O; s; sp; r;
     end
     
     methods
-        function obj = environment(T)
+        function obj = environment(T,O)
             obj.s = 1;
             obj.sp = 1;
             obj.T = T;
         end
         
-        function obj = step(obj, action)
-            obj.s = obj.T(obj.s,action);
+        function obj = step(obj, action, params)
+            if action~=7
+                obj.s = obj.T(obj.s, action);
+                obj.r = action;
+            end
         end
             
     end
--- environments/env2/main.m
+++ environments/env2/main.m
@@ -4,3 +4,26 @@ load('T');
 
 % create the environment with a Given Transition Matrix
 env = environment(T);
+
+% History of transitions (s,a,r,sp);
+H = [];
+for i = 1:100
+    actions = randperm(6);
+    s = env.s;
+    a = actions(1);
+    p = randn;
+    env = env.step(a);
+    r = env.r;
+    sp = env.s;
+    H = [H; s,a,p,r,sp];
+end
+
+% Plot History
+figure(1);
+subplot(2,2,1);
+scatter(1:100, H(:,2)); title('actions taken'); box on;
+subplot(2,2,2);
+scatter(1:100, H(:,1)); title('states visited'); box on;
+subplot(2,2,3);
+plot(H(:,3)); title('rewards returned'); box on;
+
